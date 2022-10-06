243a94e40f36c026f0e89d48d5915179ce4d6620
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-06 14:38:43 -0400

    external functions

--- funtions/function.js
+++ funtions/function.js
@@ -3,6 +3,9 @@
 //}
 
 const addTwo = require("./external.js")
+const subtract = require("./minus.js")
 
-const sum = addTwo(3,4)
-console.log(sum)
+//const sum = addTwo(3,4)
+//console.log(sum)
+const total = subtract(10,5)
+console.log(total * 2)
